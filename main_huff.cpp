2621ff6a3dc04e637d532c25eae5c479131d4cb0
Aaron Cohn <acohn@cogswell.edu>
2013-03-08 23:07:22 -0800

    renamed main to main_huff

--- main_huff.cpp
+++ main_huff.cpp
@@ -20,6 +20,8 @@ namespace po = boost::program_options;
 
 int main(int argc, char **argv)
 {	
+	string infile, outfile;
+
 	// define command-line options
 	po::options_description desc("Allowed options");
 	desc.add_options()
@@ -29,24 +31,41 @@ int main(int argc, char **argv)
 	;
 
 	// parse the command-line into a map
-	po::variables_map vm;
 	try {
+		po::variables_map vm;
 		po::store(po::parse_command_line(argc, argv, desc), vm);
-	} catch (std::exception e) { 
-		cout << desc; 
+		po::notify(vm);
+		if (vm.count("h"))
+		{
+			cout << desc << endl;
+			return 1;
+		}
+		if (vm.count("i"))
+			infile = vm["i"].as<string>();
+		if (vm.count("o"))
+			outfile = vm["o"].as<string>();
+	} 
+	catch (std::exception e) { 
+		cout << "Error in command line. See description below.\n" 
+			 << desc << endl; 
 		return 1; 
 	}
 
-	po::notify(vm);
-
-	if (vm.count("h"))
-	{
-		cout << desc << endl;
-		return 1;
+	if (infile.empty())
+		infile = PromptString("Enter path of file to be compressed: ");
+	
+	if (outfile.empty())
+	{	// use same name as infile, replace extension with .hf
+		outfile = infile;
+		auto dot = outfile.find_last_of('.');
+		outfile.replace(dot, infile.length() - 1, ".hf");
 	}
 
-	string infile, outfile;
-	HuffTree::unhuff(infile, outfile);
-
+	if (!HuffTree::huff(infile, outfile))
+	{
+		cout << "There was a problem reading the input file.";
+		return 1;
+	}
+	
 	return 0;
 }
